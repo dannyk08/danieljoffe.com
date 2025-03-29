e18908c6b91950302f843887b1c8fb938798da71
Daniel Joffe <9543755+dannyk08@users.noreply.github.com>
2025-03-29 01:30:40 -0700

    added logo; fixed experience descriptinos

--- apps/shell/src/app/shared/Logo.tsx
+++ apps/shell/src/app/shared/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex items-center gap-2">
       <Link to="/" className="text-2xl font-bold">
-        <img src="/assets/favicon.svg" alt="Logo" className="w-10 h-10" />
+        <img src="/assets/logo.svg" alt="Logo" className="h-10" />
       </Link>
     </div>
   );
